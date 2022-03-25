de0d9da51096846dd4d7e3026eb82b2cef79b3f8
niklas <MHLimM1414>
2022-03-25 18:02:17 +0100

    h01 pdf

--- blatt01/a03-04.py
+++ blatt01/a03-04.py
@@ -7,7 +7,7 @@ import numpy as np
 nums_n = [1, 2, 5, 10, 100, 1000, 100000]
 
 
-def recheck_summe(f, a, b, n):
+def rechteck_summe(f, a, b, n):
     h = (b - a) / n
     sum = 0
     for i in range(n):
@@ -37,7 +37,7 @@ def run():
     print('∫ 1/x² dx from 1/10 to 10:')
     for n in nums_n:
         print('für n=' + str(n))
-        print(recheck_summe(f1, 0.1, 10, n))
+        print(rechteck_summe(f1, 0.1, 10, n))
         print(trapezregel(f1, 0.1, 10, n))
         print()
 
@@ -45,7 +45,7 @@ def run():
     print('∫ ln(x) dx from 1 to 2:')
     for n in nums_n:
         print('für n=' + str(n))
-        print(recheck_summe(f2, 1, 2, n))
+        print(rechteck_summe(f2, 1, 2, n))
         print(trapezregel(f2, 1, 2, n))
         print()
 
@@ -63,9 +63,6 @@ def run():
 
 def vektor2(f, a, b, n):
     h = (b - a) / n
-    # v = np.array([ a+i*h for i in range(1, n)])
-    # v2 = np.array([f(x) for x in v])
-    # v2 = np.array([f(a+i*h) for i in range(1, n)])
     v = np.array([a + i * h for i in range(1, n)])
     v2 = f(v)
     s = np.sum(v2) * 2 + f(a) + f(b)
@@ -89,40 +86,37 @@ def run2():
     print('∫ 1/x² dx from 1/10 to 10:')
     t = time()
     for n in nums_n:
-        print('für n=' + str(n))
-        print(time() - t)
+        print('für n =', n)
         t = time()
-        print("recheck_summe", recheck_summe(f1, 0.1, 10, n))
-        print(time() - t)
+        print("rechteck_summe:", rechteck_summe(f1, 0.1, 10, n))
+        print("time for rechtecksumme:", time() - t)
         t = time()
         print("traprezregel", trapezregel(f1, 0.1, 10, n))
-        print(time() - t)
+        print("time for trapezregel:", time() - t)
         t = time()
         print("v1", vektor(f1, 0.1, 10, n))
-        print(time() - t)
+        print("time for vektor v1:", time() - t)
         t = time()
         print("v2", vektor2(f1, 0.1, 10, n))
-        print(time() - t)
-        t = time()
+        print("time for vektor v2:", time() - t)
         print()
 
     print()
     print('∫ ln(x) dx from 1 to 2:')
     for n in nums_n:
-        print('für n=' + str(n))
-        print(time() - t)
+        print('für n =', n)
         t = time()
-        print("summ", recheck_summe(f2, 1, 2, n))
-        print(time() - t)
+        print("summ", rechteck_summe(f2, 1, 2, n))
+        print("time for rechtecksumme:", time() - t)
         t = time()
         print("trapez", trapezregel(f2, 1, 2, n))
-        print(time() - t)
+        print("time for trapezregel:", time() - t)
         t = time()
         print("v1", vektor(f2, 1, 2, n))
-        print(time() - t)
+        print("time for vektor v1:", time() - t)
         t = time()
         print("v2", vektor2(f2, 1, 2, n))
-        print(time() - t)
+        print("time for vektor v2:", time() - t)
         t = time()
         print()
 
@@ -130,4 +124,4 @@ def run2():
 if __name__ == '__main__':
     run2()
 
-# Irgendwie ist aber die Funktion vektor nicht schneller als trapezregel.
+# Man muss darauf achten, das man die numpy Vektoren nimmt, sonst ist die Vektorart nicht schneller.
--- blatt01/a05.py
+++ blatt01/a05.py
@@ -1,2 +1,100 @@
 # Aufgabe 5
+import numpy
 
+
+# Teil a 2
+def wurzel(k, x, x0):
+    return y(k, x, x0)
+
+
+def a(k, x, x0):
+    if k > 0:
+        return ((3 / (2 * k)) - 1) * ((x / x0) - 1) * a(k - 1, x, x0)
+    return numpy.sqrt(x0)
+
+
+def y(k, x, x0):
+    if k > 0:
+        return y(k - 1, x, x0) + a(k, x, x0)
+    return numpy.sqrt(x0)
+
+
+# Man braucht 4 bzw 9 Iterationen, um eine Abweichung kleiner als 0,005 zu haben.
+
+
+# Teil b
+def heron(k, x, y0):
+    if k > 0:
+        return (heron(k - 1, x, y0) + (x / heron(k - 1, x, y0))) / 2
+    return y0
+
+
+def run():
+    print()
+    print("run()")
+    w = numpy.sqrt(2)
+    b1 = 1
+    b2 = 1
+    for k in range(1, 100):
+        w1 = wurzel(k, 2, 1)
+        w2 = wurzel(k, 2, 4)
+        if numpy.abs(w1 - w) < 0.005 and b1 == 1:
+            print()
+            print(w1)
+            print(k, "versuche um Abweichung kleiner als 0.005")
+            b1 = 0
+        if numpy.abs(w2 - w) < 0.005 and b2 == 1:
+            print()
+            print(w2)
+            print(k, "versuche um Abweichung kleiner als 0.005")
+            b2 = 0
+        if b1 == 0 and b2 == 0:
+            w1 = wurzel(20, 2, 1)
+            w2 = wurzel(20, 2, 4)
+            print()
+            print("√2:", w)
+            print("w1:", w1)
+            print("w2:", w2)
+            print("Abweichung w1:", numpy.abs(w1 - w))
+            print("Abweichung w2:", numpy.abs(w2 - w))
+            break
+
+
+def run2():
+    print()
+    print("run2()")
+    w = numpy.sqrt(2)
+    b1 = 1
+    b2 = 1
+    for k in range(1, 100):
+        w1 = heron(k, 2, 1)
+        w2 = heron(k, 2, 4)
+        if numpy.abs(w1 - w) < 0.005 and b1 == 1:
+            print()
+            print(w1)
+            print(k, "versuche um Abweichung kleiner als 0.005")
+            b1 = 0
+        if numpy.abs(w2 - w) < 0.005 and b2 == 1:
+            print()
+            print(w2)
+            print(k, "versuche um Abweichung kleiner als 0.005")
+            b2 = 0
+        if b1 == 0 and b2 == 0:
+            w1 = heron(20, 2, 1)
+            w2 = heron(20, 2, 2)
+            print()
+            print("√2:", w)
+            print("w1:", w1)
+            print("w2:", w2)
+            print("Abweichung w1:", numpy.abs(w1 - w))
+            print("Abweichung w2:", numpy.abs(w2 - w))
+            break
+
+
+if __name__ == '__main__':
+    run()
+    run2()
+
+# Nach 20 Iterationen ist das Heron-Verfahren um ein vieles genauer, wie das Verfahren aus dem ersten Aufgabenteil.
+# Das Heron-Verfahren hatte schon nach 2 bzw 4 Iterationen eine kleinere Abweichung wie 0,005.
+# Allerdings wird der Aufwand für hohe Iterationen sehr hoch.
