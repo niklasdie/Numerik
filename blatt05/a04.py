90a362d12ffbe361db70f5252847ea39fd21b7e4
niklas <MHLimM1414>
2022-04-27 15:49:09 +0200

    h05

--- blatt05/a04.py
+++ blatt05/a04.py
@@ -1,3 +1,4 @@
+from time import time
 import numpy as np
 
 
@@ -24,34 +25,77 @@ def qr_decomposition(A: np.ndarray):
     return Q[:n].T, np.triu(R[:n])
 
 
-def vorwaerts(Q, b):
-    # Q*y=b
+def zerlegung(A):
+    lu = A.copy()
+    p = []
+    for spalte in range(lu.shape[0] - 1):
+        max_element = abs(lu[spalte][spalte])
+        max_element_pos = spalte
+        for zeile in range(spalte, lu.shape[0]):
+            if (max_element < abs(lu[zeile][spalte])):
+                max_element = abs(lu[zeile][spalte])
+                max_element_pos = zeile
+
+        # Zeilen tauschen
+        tmp = np.copy(lu[spalte])
+        lu[spalte] = lu[max_element_pos]
+        lu[max_element_pos] = tmp
+        p.append(max_element_pos + 1)
+
+        for y in range(spalte + 1, lu.shape[0]):
+            div = float(lu[y][spalte] / lu[spalte][spalte])
+            if div != 0:
+                lu[y][spalte] = div
+                for x in range(spalte + 1, lu.shape[0]):
+                    lu[y][x] = lu[y][x] - lu[spalte][x] * div
+            else:
+                lu[y][spalte] = 0
+
+    # print(f"LU =\n{lu}") # not printing for cleaner output
+    # print(f"p = {p}") # not printing for cleaner output
+    return np.array(lu), np.array(p)
+
+
+def permutation(p, u):
+    c = np.copy(u)
+    for x in range(len(p)):
+        tmp = np.copy(c[x])
+        c[x] = c[p[x] - 1]
+        c[p[x] - 1] = tmp
+    # print(f"Pu = {c}") # not printing for cleaner output
+    return np.array(c)
+
+
+def vorwaerts(LU, b):
+    # L*y=b
     y = []
-    for x in range(Q.shape[0]):
+    for x in range(LU.shape[0]):
         res = b[x]
         for r in range(len(y)):
             if x == r:
                 res -= y[r]
             else:
-                res -= (Q[x][r] * y[r])
+                res -= (LU[x][r] * y[r])
         y.append(res)
-    print(f"y = {y}")
-    return np.array(y)
+    y = np.array(y)
+    # print(f"y = {y}") # not printing for cleaner output
+    return y
 
 
-def rueckwaerts(R, y):
-    # R*b=y
+def rueckwaerts(LU, y):
+    # LU*b=y
     b = []
-    for x in range(R.shape[0] - 1, -1, -1):
+    for x in range(LU.shape[0] - 1, -1, -1):
         res = y[x]
         if len(b) == 0:
-            res /= R[x][x]
+            res /= LU[x][x]
         else:
             for r in range(0, len(b)):
-                res -= (R[x][R.shape[1] - r - 1] * b[len(b) - r - 1])
-            res /= R[x][x]
+                res -= (LU[x][LU.shape[1] - r - 1] * b[len(b) - r - 1])
+            res /= LU[x][x]
         b.insert(0, res)
-    print("x = " + str(b))
+    b = np.array(b)
+    # print("x = " + str(b)) # not printing for cleaner output
     return b
 
 
@@ -96,10 +140,28 @@ if __name__ == '__main__':
         print(f"\nn={i}")
         A, b = baueBeispiel(i)
         print(f"A = \n{A}")
+        t = time()
         Q, R = qr_decomposition(A)
+        hh = time() - t
         print(f"Q = \n{Q}")
         print(f"R = \n{R}")
+        print(f"time for Householder: {hh}s")
+        t = time()
         y = np.linalg.solve(Q, b)
-        print(f"y = {y}")
         x = np.linalg.solve(R, y)
-        print(f"x = {x}")
+        shh = time() - t
+        print(f"y = {y}")
+        print(f"QR: x = {x}")
+        print(f"time for solving Householder: {shh}s")
+        t = time()
+        LU, p = zerlegung(A)
+        lu = time() - t
+        print(f"time for LU: {lu}s")
+        t = time()
+        c = permutation(p, b)
+        x = rueckwaerts(LU, vorwaerts(LU, c))
+        print(f"LU: x = {x}")
+        slu = time() - t
+        print(f"time for solving LU: {slu}s")
+        print(f"time difference Householder to LU: {hh - lu}s")
+        print(f"time difference solving Householder to LU: {shh - slu}s")
