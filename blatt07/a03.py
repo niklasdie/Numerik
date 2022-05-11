bc58a10eb76d8f41fc720ca10cd733373f9f6a49
niklas <MHLimM1414>
2022-05-11 18:47:46 +0200

    h07

--- blatt07/a03.py
+++ blatt07/a03.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from math import sqrt
 
 from scipy.sparse import spdiags, csr_matrix
 import matplotlib.animation as animation
@@ -110,14 +111,77 @@ def sign(t):
     if t >= 0:
         return 1
     else:
-        return 0
+        return -1
+
+
+def jacobi(a, tol=1.0e-3):
+    def maxElem(a):  # groesstes nicht diagonal Element
+        n = len(a)
+        max_elem = 0.0
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if abs(a[i, j]) >= max_elem:
+                    max_elem = abs(a[i, j])
+                    k = i
+                    l = j
+        return max_elem, k, l
+
+    def rotate(a, p, k, l):  # rotieren fuer a[k,l] = 0
+        n = len(a)
+        aDiff = a[l, l] - a[k, k]
+        if abs(a[k, l]) < abs(aDiff) * 1.0e-36:
+            t = a[k, l] / aDiff
+        else:
+            phi = aDiff / (2.0 * a[k, l])
+            t = 1.0 / (abs(phi) + sqrt(phi ** 2 + 1.0))
+            if phi < 0.0: t = -t
+        c = 1.0 / sqrt(t ** 2 + 1.0)
+        s = t * c
+        tau = s / (1.0 + c)
+        temp = a[k, l]
+        a[k, l] = 0.0
+        a[k, k] = a[k, k] - t * temp
+        a[l, l] = a[l, l] + t * temp
+        for i in range(k):  # fuer i < k
+            temp = a[i, k]
+            a[i, k] = temp - s * (a[i, l] + tau * temp)
+            a[i, l] = a[i, l] + s * (temp - tau * a[i, l])
+        for i in range(k + 1, l):  # fuer k < i < l
+            temp = a[k, i]
+            a[k, i] = temp - s * (a[i, l] + tau * a[k, i])
+            a[i, l] = a[i, l] + s * (temp - tau * a[i, l])
+        for i in range(l + 1, n):  # fuer i > l
+            temp = a[k, i]
+            a[k, i] = temp - s * (a[l, i] + tau * temp)
+            a[l, i] = a[l, i] + s * (temp - tau * a[l, i])
+        for i in range(n):  # Update transformation matrix
+            temp = p[i, k]
+            p[i, k] = temp - s * (p[i, l] + tau * p[i, k])
+            p[i, l] = p[i, l] + s * (temp - tau * p[i, l])
+
+    n = len(a)
+    maxRot = 5 * (n ** 2)
+    p = np.identity(n) * 1.0
+    for i in range(maxRot):
+        max_elem, k, l = maxElem(a)
+        if max_elem < tol:
+            return np.diagonal(a), p
+        rotate(a, p, k, l)
+    print('Jacobi-Verfahren konvergiert nicht!')
 
 
 m = 10
 
 if __name__ == '__main__':
     np.set_printoptions(precision=3, suppress=True)
-    A = Ablock(m)
-    v = ew_exakt(m)
-    plotev(v)
-    animev(v)()
+    A, b = system(m)
+    A = csr_matrix.toarray(A)
+    print(f"A = {A}")
+    v = np.linalg.eig(A)
+    print(f"v = {v}")
+    plotev(v[0])
+    animev(v[0])()
+    v = jacobi(A)
+    print(f"v = {v}")
+    plotev(v[0])
+    animev(v[0])()
