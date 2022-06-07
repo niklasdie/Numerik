d4a88a7b0b358dffcd74978b56e847c375c25226
niklas <MHLimM1414>
2022-06-07 11:18:09 +0200

    h11

--- blatt11/a01.py
+++ blatt11/a01.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.integrate as integrate
+from matplotlib import pyplot as plt
 
 
 # Aufgabe 1
@@ -15,6 +16,7 @@ def a_1(a, b):
 
 
 def b_1(m, a, b, f):
+    plt.plot(linspace, f(linspace))
     l = (b - a) / m
     tmp = 0
     for i in range(0, m + 1):
@@ -22,12 +24,15 @@ def b_1(m, a, b, f):
             tmp += f(a + i * l)
         else:
             tmp += 2 * f(a + i * l)
+        plt.plot(a + i * l, f(a + i * l), marker="o", color="red")
     trapez = (l / 2) * tmp
     print(f"trapez = {trapez}")
+    plt.show()
     return trapez
 
 
 def c_1(m, a, b):
+    plt.plot(linspace, f(linspace))
     m *= 2
     l = (b - a) / m
     temp = 0
@@ -38,20 +43,26 @@ def c_1(m, a, b):
         else:
             k = 2
         temp += k * f(a + i * l)
+        plt.plot(a + i * l, f(a + i * l), marker="o", color="red")
     simpson = (l / 3) * temp
     print(f"simpson = {simpson}")
+    plt.show()
     return simpson
 
 
-def d_1(a, b):
-    gauss = (8 / 9) * f(0) + (5 / 9) * f(np.sqrt(3 / 5)) + f(1)
+def d_1(a, b, f):
+    gauss = beta_umrechnen(a, b, (8 / 9)) * f(x_umrechnen(a, b, 0)) + (5 / 9) * f(x_umrechnen(a, b, np.sqrt(3 / 5))) \
+            + f(x_umrechnen(a, b, 1))
     print(f"gauss = {gauss}")
     return gauss
 
 
-def umrechnen(a, b, x, beta):
-    x = ((b - a) / 2) * x + ((b + a) / 2)
-    beta = 0  # TODO
+def x_umrechnen(a, b, x):
+    return ((b - a) / 2) * x + ((b + a) / 2)
+
+
+def beta_umrechnen(a, b, beta):
+    return ((b - a) / 2) * beta
 
 
 def skalar(f, g, a, b):
@@ -61,7 +72,7 @@ def skalar(f, g, a, b):
 # Aufgabe 2
 
 def g(x):
-    return np.sin(np.pi * x**2)
+    return np.sin(np.pi * x ** 2)
 
 
 def divDif(p):
@@ -79,7 +90,7 @@ def a_2(a, b, f, ordnung):
     T = np.zeros(shape=(len(ordnung)), dtype=np.float32)
     l = np.zeros(shape=(len(ordnung)), dtype=np.float32)
     for m in range(0, len(ordnung)):
-        T[m] = b_1(2 ** m + 1, a, b, f)
+        T[m] = b_1(2 ** m, a, b, f)
         l[m] = 2 ** m
     l = l[::-1]
     p = np.c_[l, T]
@@ -93,8 +104,15 @@ def b_2(a, b, f, ordnung):
     T = np.zeros(shape=(len(ordnung)), dtype=np.float32)
     l = np.zeros(shape=(len(ordnung)), dtype=np.float32)
     for m in range(0, len(ordnung)):
-        T[m] = b_1(2 ** m + 1, a, b, f)
-        l[m] = 2 ** m
+        if m == 0:
+            l[0] = 2
+            T[m] = b_1(l[0].astype(np.int64), a, b, f)
+        elif m == 1:
+            l[1] = 3
+            T[m] = b_1(l[1].astype(np.int64), a, b, f)
+        else:
+            l[m] = l[m - 2] * 2
+            T[m] = b_1(l[m].astype(np.int64), a, b, f)
     l = l[::-1]
     p = np.c_[l, T]
     sol = np.diag(divDif(p))
@@ -103,8 +121,10 @@ def b_2(a, b, f, ordnung):
     return sol
 
 
+linspace = np.linspace(-1, 1, 1000)
+
 if __name__ == '__main__':
-    np.set_printoptions(precision=5, suppress=True)
+    np.set_printoptions(precision=3, suppress=True)
     print("\nAufgabe 1:\n")
     a = 0
     b = 1
@@ -113,8 +133,8 @@ if __name__ == '__main__':
     print(f"abs. Fehler summierte Trapez-Regel: {np.abs(exact - trapez)}\n")
     simpson = c_1(4, a, b)
     print(f"abs. Fehler summierte Simpson-Regel: {np.abs(exact - simpson)}\n")
-    # gauss = d_1()
-    # print(f"abs. Fehler Gauß-Verfahren: {np.abs(exact - gauss)}\n")
+    gauss = d_1(a, b, f)
+    print(f"abs. Fehler Gauß-Verfahren: {np.abs(exact - gauss)}\n")
 
     print("\nAufgabe 2:\n")
     a = -1
@@ -122,7 +142,7 @@ if __name__ == '__main__':
     print("a)")
     sol = a_2(a, b, g, ordnung)
     exact = integrate.quad(g, a, b)[0]
-    print(f"abs. Fehler Romberg: {np.abs(exact - sol[len(sol)-1])}\n")
+    print(f"abs. Fehler Romberg: {np.abs(exact - sol[len(sol) - 1])}\n")
 
     print("\nb)")
     sol = b_2(a, b, g, ordnung)
